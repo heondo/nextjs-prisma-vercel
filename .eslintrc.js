724b00b7cb53edfbe85ce24a81670a660cc3da22
heondo kim <heondokim@gmail.com>
2021-06-03 00:01:08 -0700

    husky is always funky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,82 @@
 module.exports = {
   env: {
     browser: true,
-    node: true,
-    es2020: true,
+    es6: true,
   },
-  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
     ecmaFeatures: {
       jsx: true,
     },
+    ecmaVersion: 2019,
+    sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'react', 'prettier'],
-  extends: [
-    'airbnb',
-    'airbnb/hooks',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'prettier/react',
-  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  plugins: ['import', 'jsx-a11y', 'react', 'react-hooks', 'prettier'],
+  extends: ['airbnb', 'prettier'],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx'] }],
-    'import/extensions': 'off',
-    'react/prop-types': 'off',
-    'jsx-a11y/anchor-is-valid': 'off',
-    'react/jsx-props-no-spreading': ['error', { custom: 'ignore' }],
     'prettier/prettier': 'error',
-    'react/no-unescaped-entities': 'off',
-    'import/no-cycle': [0, { ignoreExternal: true }],
-    'prefer-const': 'off',
-    // needed because of https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/docs/rules/no-use-before-define.md#how-to-use & https://stackoverflow.com/questions/63818415/react-was-used-before-it-was-defined
-    'no-use-before-define': 'off',
-    '@typescript-eslint/no-use-before-define': [
+    camelcase: 'off',
+    'import/prefer-default-export': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/no-unused-prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/require-default-props': 'off',
+    'import/extensions': [
       'error',
-      { functions: false, classes: false, variables: true },
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        js: 'never',
+        jsx: 'never',
+      },
+    ],
+    quotes: 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
     ],
   },
-  settings: {
-    'import/resolver': {
-      'babel-module': {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      },
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        paths: ['src'],
+  overrides: [
+    {
+      files: '**/*.+(ts|tsx)',
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint/eslint-plugin'],
+      extends: ['plugin:@typescript-eslint/recommended', 'prettier'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'no-use-before-define': [0],
+        '@typescript-eslint/no-use-before-define': [1],
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/quotes': [
+          2,
+          'backtick',
+          {
+            avoidEscape: true,
+          },
+        ],
       },
     },
+  ],
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: '.',
+      },
+    },
+    react: {
+      version: 'detect',
+    },
   },
-}
+};
