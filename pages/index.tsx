9218b4b784d676400dc8d7543490a08f7916c972
heondo kim <heondokim@gmail.com>
2021-06-02 23:11:31 -0700

    did not work

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ const Blog: React.FC<Props> = (props) => {
   return (
     <Layout>
       <div className="page">
-        <h1>Public Feed</h1>
+        <h1 className="text-xs">Public Feed</h1>
         <main>
           {props.feed.map((post) => (
             <div key={post.id} className="post">
